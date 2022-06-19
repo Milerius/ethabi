8cc1d8a38356c940d35299be31ac02cfe90a3d68
milerius <rmscastle@gmail.com>
2022-06-19 12:31:03 +0200

    improvements: refactor a bit

--- cmake/Doxygen.cmake
+++ cmake/Doxygen.cmake
@@ -5,7 +5,7 @@ if(${PROJECT_NAME}_ENABLE_DOXYGEN)
     set(DOXYGEN_OUTPUT_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/docs)
 
     find_package(Doxygen REQUIRED dot)
-    doxygen_add_docs(doxygen-docs ${PROJECT_SOURCE_DIR})
+    doxygen_add_docs(doxygen-docs ${PROJECT_SOURCE_DIR}/include)
 
     verbose_message("Doxygen has been setup and documentation is now available.")
 endif()
